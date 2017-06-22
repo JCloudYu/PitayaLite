9f048041dd6abc7234168b9ec50d10c8b48e10eb
JCloudYu <j.cloud.yu@gmail.com>
2017-06-22 16:25:26 +0800

    Update pitaya.config.php to its default configurations

--- pitaya.config.php
+++ pitaya.config.php
@@ -1,10 +1,10 @@
 <?php
-	const SYS_LOG_DIR		= __ROOT . '/log';
-	const ENV_ATTACH_LEVEL	= 2;
+	const SYS_LOG_DIR			= __ROOT . '/log';
+	const ENV_ATTACH_LEVEL		= 0;
 	
-	const SYS_TIMEZONE		= 'Asia/Taipei';
-	const THROW_EXCEPTIONS	= FALSE;
-	const LOG_EXCEPTIONS	= TRUE;
+	const SYS_TIMEZONE			= 'UTC';
+	const THROW_EXCEPTIONS		= FALSE;
+	const LOG_EXCEPTIONS		= TRUE;
 	
-	const DEBUG_MODE			= TRUE;
+	const DEBUG_MODE			= FALSE;
 	const DEBUG_VIEWPORT_WIDTH	= 350;
