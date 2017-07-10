985f542caa9f0a3df89ef0b4077e9b33435a6934
JCloudYu <j.cloud.yu@gmail.com>
2017-07-11 01:11:53 +0800

    Minor modifications

--- pitaya.config.php
+++ pitaya.config.php
@@ -1,10 +1,8 @@
 <?php
+	const ENV_ATTACH_DEPTH		= 0;				// Default: 0
+	const SYS_TIMEZONE			= 'UTC';			// Default: UTC
+	const DEBUG_MODE			= FALSE;			// Default: FALSE
+	const THROW_EXCEPTIONS		= FALSE;			// Default: FALSE
+	const LOG_EXCEPTIONS		= TRUE;				// Default: TRUE
+	const DEBUG_DIALOG_WIDTH	= 350;				// Default:350
 	const SYS_LOG_DIR			= __ROOT . '/log';
-	const ENV_ATTACH_DEPTH		= 0;
-	
-	const SYS_TIMEZONE			= 'UTC';
-	const THROW_EXCEPTIONS		= FALSE;
-	const LOG_EXCEPTIONS		= TRUE;
-	
-	const DEBUG_MODE			= FALSE;
-	const DEBUG_DIALOG_WIDTH	= 350;
