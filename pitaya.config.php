02e1183e0b863c27eb4397467166468a34a92570
JCloudYu <j.cloud.yu@gmail.com>
2017-07-11 00:28:18 +0800

    Adapt to kernel's latest modification

--- pitaya.config.php
+++ pitaya.config.php
@@ -1,10 +1,10 @@
 <?php
 	const SYS_LOG_DIR			= __ROOT . '/log';
-	const ENV_ATTACH_LEVEL		= 0;
+	const ENV_ATTACH_DEPTH		= 0;
 	
 	const SYS_TIMEZONE			= 'UTC';
 	const THROW_EXCEPTIONS		= FALSE;
 	const LOG_EXCEPTIONS		= TRUE;
 	
 	const DEBUG_MODE			= FALSE;
-	const DEBUG_VIEWPORT_WIDTH	= 350;
+	const DEBUG_DIALOG_WIDTH	= 350;
