8244950c2f00a07819f00b1095645b6dc26d2aa8
JCloudYu <j.cloud.yu@gmail.com>
2017-07-11 00:18:59 +0800

    Adapt to kernel's latest modification

--- gateway.php
+++ gateway.php
@@ -27,30 +27,6 @@
 
 	
 	require_once __PITAYA_LIB_PATH . '/pitaya.php';
+	require_once __PITAYA_LIB_PATH . '/init-lite.php';
 	
-	Pitaya([
-		'space-root' => __ROOT,
-		'packages' => [
-			'broot'		=> @constant( 'BASIS_PATH' ) ?: __ROOT.'/app',
-			'ext'		=> __ROOT.'/ext',
-			'data'		=> __ROOT.'/data'
-		],
-		'module-packages' => [
-			'ext.modules'
-		],
-		'boot-scripts' => [
-			'broot.boot', 'ext.boot', 'root.boot'
-		],
-		'default-basis'	=> 'main',
-		'debug-mode' => @constant( 'DEBUG_MODE' ),
-		'debug-console-width' => @constant( 'DEBUG_VIEWPORT_WIDTH' ),
-		'system-timezone' => @constant( 'SYS_TIMEZONE' ),
-		'throw-exceptions' => @constant( 'THROW_EXCEPTIONS' ),
-		'log-exceptions' => @constant( 'LOG_EXCEPTIONS' ),
-		'attach-depth' => @constant( 'ENV_ATTACH_DEPTH' ) ?: @constant( 'ENV_ATTACH_LEVEL' ) ?: 0,
-		
-		
-		'leading-modules' => [],
-		'tailing-modules' => [],
-		'log-dir' => @constant( 'SYS_LOG_DIR' ) ?: __ROOT . '/log'
-	]);
+	Pitaya()->execute();
